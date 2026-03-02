cb749d4999a63b4c104b2b166b151ac513c54d26
jotacee012 <jcjoaocesar0@gmail.com>
2026-03-01 23:01:56 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasComprasJC/Views/ListaProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasComprasJC.Views;
+
+public partial class ListaProduto : ContentPage
+{
+	public ListaProduto()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppMinhasComprasJC/Views/NovoProduto.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppMinhasComprasJC.Views;
+
+public partial class NovoProduto : ContentPage
+{
+	public NovoProduto()
+	{
+		InitializeComponent();
+	}
+}
